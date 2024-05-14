2de59ae7a86fb720c02913cba580523cdb75ce1b
SaifMalik1 <saifdon516@gmail.com>
2024-05-14 14:17:30 +0500

    add

--- src/Components/About/AboutHeader/World.jsx
+++ src/Components/About/AboutHeader/World.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const World = () => {
   return (
@@ -26,7 +26,7 @@ const World = () => {
 
       {/* ------- */}
 
-      <div className="bg-black pt-8">
+      <div className="bg-black pt-8 ">
         <p className="text-center text-white font-bold text-6xl">
           Our achievements
         </p>
@@ -34,22 +34,78 @@ const World = () => {
           speak for themselves
         </p>
 
-        <div className="grid lg:grid-cols-4 grid-cols-1 lg:px-12  gap-5 px-5 ">
-          <div className="border border-neutral-500 col-span-2 rounded-lg tracking-wide  flex flex-col lg:flex-row  gap-8 items-center pl-5  bg-neutral-800">
-            <p className="text-neutral-400 text-[21.3px]">
+        <div class="grid lg:grid-cols-4 grid-cols-1 lg:px-12 py-4 gap-4 px-5">
+          <div class="border border-neutral-500 col-span-2 rounded-lg tracking-wide flex flex-col lg:flex-row gap-8 items-center pl-5 bg-neutral-800">
+            <p class="text-neutral-400 lg:text-lg text-base">
               The number 1 brand for truly wireless, and earwear in Pakistan as
               per Q3 CY21
             </p>
-            <div className='pr-16'>
-              <img src="/img/Image-1.png" alt="" />
+            <div class="pr-16">
+              <img src="/img/Image-1.png" alt="" class="" />
             </div>
           </div>
-          <div className="border col-span-1 rounded-lg"> 2</div>
-          <div className="border col-span-1 rounded-lg">3</div>
+          <div class="border col-span-1 rounded-lg py-5 text-neutral-200 text-lg bg-neutral-800 border-neutral-500">
+            <div class="lg:px-10 px-5">
+              <img src="/img/Image-2.png" alt="" class="" />
+            </div>
+            <p class="px-2 text-center">No. 3 Earwear + Speakers global</p>
+            <p class="px-2 text-center">market as per Q3 CY21</p>
+          </div>
+          <div class="border col-span-1 rounded-lg">3</div>
+        </div>
+
+        {/* ------- */}
+
+        <div className="bg-black flex flex-col items-center pt-10 pb-24">
+          <p className="text-white text-center text-5xl font-bold">
+            What's next?
+          </p>
+          <p className="text-white text-[23px] text-center pt-3">
+            All that has happened to us is overwhelming, yes, but it pushes us
+            to do more, be
+          </p>
+          <p className="text-white text-[23px] text-center">
+            more and make more products that you can use to{" "}
+            <span className="font-bold text-neutral-400">
+              jazz up your lifestyle.
+            </span>
+          </p>
+          <p className="text-white text-[23px] pt-3 text-center">
+            So, in 2022, we've started to fearlessly rock in Pakistan with a
+            range of products that
+          </p>
+          <p className="font-bold text-white text-[23px] text-center">
+            free-spirited Pakistanis like you can truly call your own!
+          </p>
+
+          <button className="bg-green-500 text-black text-xl font-bold px-8 py-1.5 my-20">
+            EXPLORE NOW
+          </button>
+          <p className="text-5xl text-white font-bold">MADE IN PAKISTSAN</p>
+          <p className="text-white text-[20px] pt-5">
+            BY A PAKSITANI <span className="text-green-500">BRAND</span>
+          </p>
+        </div>
+
+        {/* ---- */}
+
+        <div className="px-16 py-20">
+          <marquee
+            behavior=""
+            direction="scroll"
+            scrollamount=""
+            className="text-white text-3xl font-bold"
+          >
+            <span className="text-red-500"> Pakistan's # 1 Audio Brand</span>{" "}
+            Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1
+            Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand
+            Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1
+            Audio Brand{" "}
+          </marquee>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default World
+export default World;
--- src/Components/Login/SignUp.jsx
+++ src/Components/Login/SignUp.jsx
@@ -1,74 +1,118 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup";
+import React from "react";
+import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { MdOutlineLocalShipping } from "react-icons/md";
 import { FaIdeal } from "react-icons/fa6";
 import { BsJournalCheck } from "react-icons/bs";
 const schema = yup.object().shape({
-    // firstname: yup.string().firstname().required(),
-    // lastname: yup.string().lastname().required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(8).max(32).required(),
+  // firstname: yup.string().firstname().required(),
+  // lastname: yup.string().lastname().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(32).required(),
 });
 function SignUp() {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(schema),
-    });
-    const onSubmitHandler = (data) => {
-        console.log({ data });
-        reset();
-    };
-    return (
-        <div className='pt-10 '>
-            <div className=' px-36'>
-                <h1 className='text-3xl font-semibold text-center border-b border-b-black pb-7'>CREATE AN ACCOUNT</h1>
-            </div>
-            <div>
-                <form action="" className='flex flex-col px-36 gap-4 pt-10' onSubmit={handleSubmit(onSubmitHandler)}>
-                    <label htmlFor="firstname">First Name</label>
-                    <input type="firstname" {...register("firstname")} id='firstname' className='border border-gray-400 w-1/3 p-2 ' />
-                    <p>{errors.firstname?.message}</p>
-                    <label htmlFor="secondname">Last Name</label>
-                    <input type="lastname" {...register("lastname")} id='secondname' className='border border-gray-400 w-1/3 p-2' />
-                    <p>{errors.lastname?.message}</p>
-                    <label htmlFor="email">Your Email Address</label>
-                    <input type="text" {...register("email")} id='email' className='border border-gray-400 p-2 w-1/3 ' />
-                    <p>{errors.email?.message}</p>
-                    <label htmlFor="password">Your Password</label>
-                    <input type="text" {...register("password")} id='password' className='border border-gray-400 p-2 w-1/3 ' />
-                    <p>{errors.password?.message}</p>
-                    <button className='border border-black px-8 w-56 py-3 hover:bg-red-500 hover:border-none hover:text-white'>CREATE AN ACCOUNT</button>
-                </form>
-            </div>
-            <div className='pt-32'>
-            <div className="px-16 py-10 bg-black">
-            <marquee
-               behavior=""
-               direction="scroll"
-               scrollamount=""
-               className="text-white text-3xl font-bold"
-            >
-               <span className="text-red-500"> Pakistan's # 1 Audio Brand </span>
-               Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's #
-               1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1 Audio
-               Brand Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand
-               Pakistan's # 1 Audio Brand
-            </marquee>
-         </div>
-         <div className='bg-black/90 flex md:gap-10 gap-6  text-white justify-center lg:gap-28 py-5'>
-            <div className='flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center'><MdOutlineLocalShipping className='text-red-600 text-4xl' />
-               <h1>Free Shipping</h1></div>
-            <div className='flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center'><FaIdeal className='text-red-600 text-4xl' />
-               <h1>Exclusive Deals</h1></div>
-            <div className='flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center'><BsJournalCheck className='text-red-600 text-4xl' />
-               <h1>Secure Checkout</h1></div>
-         </div>
-         <div className='bg-[#333] px-20 py-5 '>
-            <h1 className='text-gray-400 text-center lg:text-[14px] sm:text-[10px] text-[6px] '>Pakistan's No.1 audio & wearables brand. Audionic holds the widest range of wireless Earbuds, Bluetooth Speakers, home audio & Accessories. From <br /> workouts to adventures, We have you covered!</h1>
-         </div>
-            </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmitHandler = (data) => {
+    console.log({ data });
+    reset();
+  };
+  return (
+    <div className="pt-10 ">
+      <div className=" px-36">
+        <h1 className="text-3xl font-semibold text-center border-b border-b-black pb-7">
+          CREATE AN ACCOUNT
+        </h1>
+      </div>
+      <div>
+        <form
+          action=""
+          className="flex flex-col px-36 gap-4 pt-10"
+          onSubmit={handleSubmit(onSubmitHandler)}
+        >
+          <label htmlFor="firstname">First Name</label>
+          <input
+            type="firstname"
+            {...register("firstname")}
+            id="firstname"
+            className="border border-gray-400 w-1/3 p-2 "
+          />
+          <p>{errors.firstname?.message}</p>
+          <label htmlFor="secondname">Last Name</label>
+          <input
+            type="lastname"
+            {...register("lastname")}
+            id="secondname"
+            className="border border-gray-400 w-1/3 p-2"
+          />
+          <p>{errors.lastname?.message}</p>
+          <label htmlFor="email">Your Email Address</label>
+          <input
+            type="text"
+            {...register("email")}
+            id="email"
+            className="border border-gray-400 p-2 w-1/3 "
+          />
+          <p>{errors.email?.message}</p>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="text"
+            {...register("password")}
+            id="password"
+            className="border border-gray-400 p-2 w-1/3 "
+          />
+          <p>{errors.password?.message}</p>
+          <button className="border border-black px-8 w-56 py-3 hover:bg-red-500 hover:border-none hover:text-white">
+            CREATE AN ACCOUNT
+          </button>
+        </form>
+      </div>
+      <div className="pt-32">
+        <div className="px-16 py-10 bg-black">
+          <marquee
+            behavior=""
+            direction="scroll"
+            scrollamount=""
+            className="text-white text-3xl font-bold"
+          >
+            <span className="text-red-500"> Pakistan's # 1 Audio Brand </span>
+            Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1
+            Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand
+            Pakistan's # 1 Audio Brand Pakistan's # 1 Audio Brand Pakistan's # 1
+            Audio Brand
+          </marquee>
         </div>
-    )
+        <div className="bg-black/90 flex md:gap-10 gap-6  text-white justify-center lg:gap-28 py-5">
+          <div className="flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center">
+            <MdOutlineLocalShipping className="text-red-600 text-4xl" />
+            <h1>Free Shipping</h1>
+          </div>
+          <div className="flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center">
+            <FaIdeal className="text-red-600 text-4xl" />
+            <h1>Exclusive Deals</h1>
+          </div>
+          <div className="flex md:flex-row flex-col items-center gap-3 sm:text-xl text-sm text-center">
+            <BsJournalCheck className="text-red-600 text-4xl" />
+            <h1>Secure Checkout</h1>
+          </div>
+        </div>
+        <div className="bg-[#333] px-20 py-5 ">
+          <h1 className="text-gray-400 text-center lg:text-[14px] sm:text-[10px] text-[6px] ">
+            Pakistan's No.1 audio & wearables brand. Audionic holds the widest
+            range of wireless Earbuds, Bluetooth Speakers, home audio &
+            Accessories. From <br /> workouts to adventures, We have you
+            covered!
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default SignUp
+export default SignUp;
